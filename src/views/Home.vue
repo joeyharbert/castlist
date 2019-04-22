35815f52b50f225ddfb1b18fb81ddd32d78c6e67
joeyharbert <joey.harbert@gmail.com>
2019-04-22 12:06:43 -0500

    Adds index action for audition days

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+
+    <div class="containter" v-for="audition in auditions">
+      <h3>{{ audition.name }}</h3>
+      <p>Show: {{ audition.show.name }}</p>
+      <p>Company: {{ audition.company.name }}</p>
+      <p>Audition Date: {{ audition.time_slots[0].start_time.substring(0, 10)}}</p>
+    </div>
   </div>
 </template>
 
@@ -8,13 +15,20 @@
 </style>
 
 <script>
-export default {
-  data: function() {
-    return {
-      message: "Welcome to Vue.js!"
-    };
-  },
-  created: function() {},
-  methods: {}
-};
+  import axios from "axios"
+
+  export default {
+    data: function() {
+      return {
+        message: "Welcome to Castlist! Audition Index",
+        auditions: []
+      };
+    },
+    created: function() {
+      axios.get("/api/auditions").then(response=> {
+        this.auditions = response.data;
+      });
+    },
+    methods: {}
+  };
 </script>
