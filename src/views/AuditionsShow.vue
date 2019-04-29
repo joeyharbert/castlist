f73229b9306999f08e634de9760a85d9b269df8b
joeyharbert <joey.harbert@gmail.com>
2019-04-29 13:28:46 -0500

    Adds actor sign up for AuditionShow

--- src/views/AuditionsShow.vue
+++ src/views/AuditionsShow.vue
@@ -19,11 +19,13 @@
       </p>
       <p>Requirements: {{ audition.requirements }}</p> <hr>
 
-      <div class="timeSlots" v-for="timeSlot in audition.time_slots">
+      <div class="timeSlots" v-for="timeSlot in timeSlots">
         <p>Start: {{ timeSlot.start_time.substring(11, 16) }}</p>
         <p>End: {{ timeSlot.end_time.substring(11, 16) }}</p>
         <p v-if="timeSlot.actor">Filled by: {{ timeSlot.actor.first_name }} {{ timeSlot.actor.last_name }} </p>
-        <p v-if="!timeSlot.actor"> Open </p>
+        <div v-if="!timeSlot.actor"> 
+          <p>Open: <button v-on:click="signUp(timeSlot)">Sign Up!</button></p> 
+        </div>
         <hr>
       </div>
 
@@ -42,6 +44,7 @@ export default {
   data: function() {
     return {
       audition: {},
+      timeSlots: [],
       loading: false
     };
   },
@@ -49,6 +52,7 @@ export default {
     this.loading = true;
     axios.get("/api/auditions/" + this.$route.params.id).then(response => {
       this.audition = response.data;
+      this.timeSlots = this.audition.time_slots
       this.loading = false;
     });
   },
@@ -68,11 +72,23 @@ export default {
       axios.patch("/api/auditions/" + audition.id, params)
       .then(response => {
         console.log(response.data);
-        })
+      })
       .catch(error => {
         this.errors = error.response.data.errors;
       });
       this.$router.push("/auditions/" + audition.id + "/live");
+    },
+    signUp: function(timeSlot) {
+      axios.patch("/api/time_slots/" + timeSlot.id)
+      .then(response => {
+        console.log(response);
+        var index = this.timeSlots.indexOf(timeSlot);
+        var newTimeSlot = response.data;
+        this.timeSlots.splice(index, 1, newTimeSlot); //replace old ts with new one
+      })
+      .catch(error => {
+        this.errors = error.response.data.errors;
+      });
     }
   }
 };
