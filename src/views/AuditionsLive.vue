c915f0ece46a0d0ad662b093e97067b5de679bf5
joeyharbert <joey.harbert@gmail.com>
2019-04-30 12:13:59 -0500

    Deletes old code from live view

--- src/views/AuditionsLive.vue
+++ src/views/AuditionsLive.vue
@@ -42,21 +42,6 @@
          </nav>
        </div>
      </div>
-
-    <!-- <h1>{{ message }}</h1>
-
-    <div class="body">
-      <div v-for="timeSlot in timeSlots" class="mid" v-if="(currentTimeSlot === timeSlot)">
-        <p>{{timeSlot.actor.first_name}}</p>
-        <p>Sort: {{timeSlot.sort}}</p>
-      </div>
-      <div class="nav">
-        <div class="left"><button v-on:click="next()">Next</button></div>
-        <div class="up"><button v-on:click="sort(currentTimeSlot, 1)">Starred</button></div>
-        <div class="right"><button v-on:click="sort(currentTimeSlot, 0)">Callback</button></div>
-        <div class="right"><button v-on:click="sort(currentTimeSlot, 'none')">X</button></div>
-      </div>
-    </div> -->
   </div>
 </template>
 
